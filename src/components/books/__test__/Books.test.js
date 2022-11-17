06a256afbf6de1cfdf1c18607610e5e02d474b9c
leahobot <leahobot@gmail.com>
2022-11-17 14:00:48 +0100

    final updates

---
+++ src/components/books/__test__/Books.test.js
@@ -0,0 +1,21 @@
+import {render, screen} from "@testing-library/react";
+import Books from "../Books";
+// import {BrowserRouter} from "react-router-dom";
+
+// const MockBooks = () => {
+// 	return (
+// 		<BrowserRouter>
+// 			<Books />
+// 		</BrowserRouter>
+// 	);
+// };
+
+// describe("Books", () => {
+// 	it("should render input element", async () => {
+// 		render(<Books />);
+// 		const searchElement = screen.getByPlaceholderText(
+// 			/Search for a book here.../i,
+// 		);
+// 		expect(searchElement).toBeInTheDocument();
+// 	});
+// });
